26a9f31d56e56add6146ce6d07e8243868c435a1
vascofg <vascofg@gmail.com>
2013-05-21 16:55:02 +0100

    Update dictionary; Initialized implementation

--- cal_proj2/Main.cpp
+++ cal_proj2/Main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdio.h>
+#include <string.h>
 using namespace std;
 
 int main()
 {
-	cout << "Hello world!" << endl;
+	ifstream is;
+	is.open("words.txt");
+	string tmp;
+	int size, tmpsize;
+	cout << "Palavra: ";
+	cin >> tmp;
+	size=tmp.size();
+	cout << size << endl;
+	vector<string> v;
+	while ( is.good() )
+	{
+		getline(is,tmp);
+		tmpsize=tmp.size();
+		if(tmpsize==size || tmpsize==size+1)
+		{
+			//cout << tmpsize << " - " << tmp << endl;
+			v.push_back(tmp);
+		}
+	}
+	/*for(int i=0;i<v.size();i++)
+		cout << v[i] << endl;*/
+	cout << "Encontradas " << v.size() << " palavras possíveis. Boa sorte." << endl;
 }
